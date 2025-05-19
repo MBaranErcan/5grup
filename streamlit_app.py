c838f85bbcaeab6e6a58b4ff06a87dfa3759b422
MBaranErcan <102927124+MBaranErcan@users.noreply.github.com>
2025-05-19 18:30:19 +0000

    modified:   requirements.txt 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
